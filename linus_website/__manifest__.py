9784159667ec038cd61c134ecdbb76f9c7d5599f
Toan Truong <toan@syncoria.com>
2022-05-24 14:42:12 -0400

    [ADD] linus_website: init

---
+++ linus_website/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Linus B2B Website",
+    'summary': """Linus B2B Website""",
+    'description': """Linus B2B Website""",
+    'author': "Syncoria Inc",
+    'website': "http://www.syncoria.com",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['base', 'website'],
+    'images': [
+    ],
+    'data': [
+
+    ],
+    'license': 'OPL-1',
+    'support': "support@syncoria.com",
+    'assets': {
+		'web.assets_frontend': [
+             'linus_website/static/src/scss/style.scss',
+             'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap',
+        ],
+    }
+}
