ace8cf2ef6385401aadd18b32b0c6a0ff23d0c7c
Toan Truong <toan@syncoria.com>
2022-05-24 14:48:49 -0400

    [REMOVE] linus_website: models dir

--- linus_website/__init__.py
+++ linus_website/__init__.py
@@ -2,5 +2,3 @@
 #    License, author and contributors information in:                         #
 #    __manifest__.py file at the root folder of this module.                  #
 ###############################################################################
-
-from . import models
