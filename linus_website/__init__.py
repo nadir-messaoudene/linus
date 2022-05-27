487624d429cf5c01665c9e9e71948845d249e0eb
Toan Truong <toan@syncoria.com>
2022-05-27 09:58:53 -0400

    [ADD] linus_website: init

---
+++ linus_website/__init__.py
@@ -0,0 +1,4 @@
+###############################################################################
+#    License, author and contributors information in:                         #
+#    __manifest__.py file at the root folder of this module.                  #
+###############################################################################
