cb5979759fc2fbea55650619d89aacb11786e653
realariful <real_ariful@hotmail.com>
2022-05-30 16:40:52 +0600

    [ADD] Linus Delivery Fedex Fix

---
+++ linus_delivery_fedex_fix/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of syncoria. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Fedex Shipping issue fix",
+    'description': "Send your shippings through Fedex and track them online fixed",
+    'category': 'Inventory/Delivery',
+    'sequence': 295,
+    'author': 'Syncoria Inc.',
+    'version': '15.0.1',
+    'website': 'http://www.syncoria.com',
+    'depends': ['delivery_fedex'],
+    'data': [
+    ],
+    'license': 'OPL-1',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
