0e4906c03225e6eb21971e975f86e1a1d48e2347
DUC SYNCORIA <duc@syncoria.com>
2022-06-01 19:45:29 -0400

    [UPDATE] complete backorder creation

--- syncoria_3plcentral/models/stock_picking.py
+++ syncoria_3plcentral/models/stock_picking.py
@@ -10,6 +10,7 @@ from odoo.tools.misc import clean_context, OrderedSet
 import requests
 import json
 
+from random import randrange
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
@@ -32,6 +33,7 @@ class StockPicking(models.Model):
              " * Done: The transfer has been processed.\n"
              " * Cancelled: The transfer has been cancelled.")
     threeplId = fields.Integer(string="3PL ID")
+    tracking_3pl = fields.Char(string="Tracking Number 3PL")
 
     def get_3pl_warehouse_from_locations(self, source_location_obj, dest_location_obj):
         warehouse_ids = []
@@ -74,7 +76,7 @@ class StockPicking(models.Model):
                 "facilityIdentifier": {
                     "id": source_warehouse
                 },
-                "referenceNum": self.name,
+                "referenceNum": self.name + str(randrange(10)),
                 "notes": '',
                 "shippingNotes": '',
                 "billingCode": "Prepaid",
@@ -104,6 +106,9 @@ class StockPicking(models.Model):
                 'Authorization': 'Bearer ' + str(instance.access_token)
                 }
             response = requests.request("POST", url, headers=headers, data=payload)
+            res_dict = self.button_validate()
+            if type(res_dict) != bool:
+                self.env['stock.backorder.confirmation'].with_context(res_dict['context']).process()
             if response.status_code == 201:
                 response = json.loads(response.text)
                 self.threeplId = response.get('readOnly').get('orderId')
@@ -129,29 +134,66 @@ class StockPicking(models.Model):
         if response.status_code == 200:
             response = json.loads(response.text)
             print(response)
-            #Is Closed 
             is_closed = response.get('readOnly').get('isClosed')
             status = response.get('readOnly').get('status')
+            fully_allocated = response.get('readOnly').get('fullyAllocated')
             print(is_closed)
             print(status)
-
-            #CANCEL ORDER
+            # CANCEL ORDER
             if is_closed and status == 2:
                 if self.state == 'push_3pl':
                     self.action_cancel()
+            # CLOSED ORDER
+            if is_closed and status == 1 and fully_allocated:
+                tracking_number = response.get('routingInfo').get('trackingNumber')
+                self.tracking_3pl = tracking_number
+                items_dict = self.get_order_item_3pl(self.threeplId)
+                dict_item_to_create_backorder_lines = {}
+                for item in items_dict:
+                    item_3pl_id = item.get('itemIdentifier').get('id')
+                    item_qty = item.get('qty')
+                    item_odoo_id = self.env['product.product'].search([('product_3pl_id', '=', item_3pl_id)])
+                    if not item_odoo_id:
+                        raise UserError('Can not find the product with 3pl id: ' + str(item_3pl_id))
+                    res_item = self.move_line_ids_without_package.filtered(lambda l: l.product_id == item_odoo_id)
+                    if not res_item:
+                        raise UserError(
+                            'Can not find move_line_ids_without_package with product_id: ' + str(item_odoo_id.id))
+                    if res_item.qty_done != item_qty:
+                        dict_item_to_create_backorder_lines[item_odoo_id.id] = res_item.qty_done - item_qty
+                        res_item.write({'qty_done': item_qty})
+                if dict_item_to_create_backorder_lines:
+                    new_backorder = self.copy()
+                    new_backorder.threeplId = ''
+                    list_product = list(dict_item_to_create_backorder_lines.keys())
+                    for line in new_backorder.move_ids_without_package:
+                        if line.product_id.id not in list_product:
+                            line.unlink()
+                        else:
+                            line.product_uom_qty = dict_item_to_create_backorder_lines.get(line.product_id.id)
+                    self.message_post(
+                        body=_(
+                            'The backorder <a href=# data-oe-model=stock.picking data-oe-id=%d>%s</a> has been created.') % (
+                                 new_backorder.id, new_backorder.name))
+                self.state = 'done'
+        else:
+            raise UserError(response.text)
 
-            #CLOSED ORDER        
-            # if is_closed and status == 0:
-            #TODO: Replace this line by above line once going live
-            # if not is_closed and status == 0:
-                #TODO: Closed (Complete) -> Validate and Create BackOrder (If remain)
+    def get_order_item_3pl(self, order_id):
+        instance = self.env['instance.3pl'].search([], limit=1)
+        url = "https://secure-wms.com/orders/{}/items".format(order_id)
 
-            
-            
-            
-
-
-
-
+        headers = {
+            'Host': 'secure-wms.com',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'application/hal+json',
+            'Authorization': 'Bearer ' + str(instance.access_token)
+        }
+        response = requests.request("GET", url, headers=headers, data={})
+        if response.status_code == 200:
+            response = json.loads(response.text)
+            print(json.dumps(response, indent=2))
+            return response.get('_embedded').get('http://api.3plCentral.com/rels/orders/item')
         else:
             raise UserError(response.text)
