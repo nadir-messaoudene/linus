c2897403b1bd00fff56ce550c6d9dc7b7ab93c7d
Toan Truong <toan@syncoria.com>
2022-06-09 14:52:20 -0400

    [ADD] linus_sale: Last Delivery Order status

---
+++ linus_sale/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Linus Bike Sale",
+    'summary': """Linus Bike Sale""",
+    'description': """Linus Bike Sale""",
+    'author': "Syncoria Inc",
+    'website': "http://www.syncoria.com",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['base', 'sale', 'sale_stock'],
+    'images': [
+    ],
+    'data': [
+        'views/sale_order_view.xml'
+    ],
+    'license': 'OPL-1',
+    'support': "support@syncoria.com",
+}
