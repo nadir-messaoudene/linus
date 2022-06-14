f5fa8c448cc5d937ae5969788de673ccf1e2d09c
ptr-syncoria <pariket@syncoria.com>
2022-06-14 18:55:33 +0530

    [FIX]: delivery freair apply base on product category

--- linus_shipping_base_product_category/models/delivery_carrier.py
+++ linus_shipping_base_product_category/models/delivery_carrier.py
@@ -20,85 +20,43 @@ class DeliveryCarrier(models.Model):
     def _get_price_available(self, order):
         self.ensure_one()
         self = self.sudo()
-        delivery_carrier_price = 0
         order = order.sudo()
-        total = weight = volume = quantity = 0
-        categ_ids = self.env['product.category']
-        total_delivery = 0.0
-        for line in order.order_line:
-            if line.state == 'cancel':
-                continue
-            if line.is_delivery:
-                total_delivery += line.price_total
-            if not line.product_id or line.is_delivery:
-                continue
-            if line.product_id.type == "service":
-                continue
-            qty = line.product_uom._compute_quantity(line.product_uom_qty, line.product_id.uom_id)
-            weight = (line.product_id.weight or 0.0) * qty
-            volume = (line.product_id.volume or 0.0) * qty
-            quantity = qty
-            categ_ids = line.product_id.categ_id
-            total = (line.price_subtotal + line.price_tax or 0.0) - total_delivery
+        categ_ids = set([rule.categ_ids for rule in self.price_rule_ids])
+        delivery_carrier_price = 0
+        for categ in categ_ids:
+            total = weight = volume = quantity = 0
+            total_delivery = 0.0
+            for line in order.order_line.filtered(lambda l: l.product_id.categ_id.id in categ.ids):
+                if line.state == 'cancel':
+                    continue
+                if line.is_delivery:
+                    total_delivery += line.price_total
+                if not line.product_id or line.is_delivery:
+                    continue
+                if line.product_id.type == "service":
+                    continue
+                qty = line.product_uom._compute_quantity(line.product_uom_qty, line.product_id.uom_id)
+                weight += (line.product_id.weight or 0.0) * qty
+                volume += (line.product_id.volume or 0.0) * qty
+                quantity += qty
+                total += (line.price_subtotal + line.price_tax or 0.0) - total_delivery
             total = self._compute_currency(order, total, 'pricelist_to_company')
-            delivery_carrier_price += self._get_price_from_order_picking(total, weight, volume, quantity, categ_ids)
-        print("delivery_carrier_price >>>>>>>>>>>>>>>>>>>", delivery_carrier_price)
+            if order.order_line.filtered(lambda l: l.product_id.categ_id.id in categ.ids):
+                delivery_carrier_price += self._get_price_from_order_picking(total, weight, volume, quantity, categ)
         return delivery_carrier_price
 
-    def _get_price_dict(self, total, weight, volume, quantity):
-        '''Hook allowing to retrieve dict to be used in _get_price_from_picking() function.
-        Hook to be overridden when we need to add some field to product and use it in variable factor from price rules. '''
-        return {
-            'price': total,
-            'volume': volume,
-            'weight': weight,
-            'wv': volume * weight,
-            'quantity': quantity
-        }
-
     def _get_price_from_order_picking(self, total, weight, volume, quantity, categ_ids):
-        print("_get_price_from_order_picking >>>>>>>>>>>>>>", self, total, weight, volume, quantity, categ_ids)
         price = 0.0
         criteria_found = False
         price_dict = self._get_price_dict(total, weight, volume, quantity)
         if self.free_over and total >= self.amount:
             return 0
-        for categ in categ_ids:
-            print("categ >>>>>>>>>>>>>>>>>>", categ)
-            for line in self.price_rule_ids.filtered(lambda rule: categ in rule.categ_ids):
-                print("line >>>>>>>>>>>>>", line)
-                test = safe_eval(line.variable + line.operator + str(line.max_value), price_dict)
-                print("test >>>>>>>>>>>>>>>>>>>>>>>", test)
-                if test:
-                    price = line.list_base_price + line.list_price * price_dict[line.variable_factor]
-                    print("price >>>>>>>>>>>>>", price)
-                    # print("(1.0 + (line.margin / 100.0)) >>>>", (1.0 + (line.margin / 100.0)))
-                    # if line.margin:
-                    #     price = price * (line.margin / 100.0)
-                    criteria_found = True
-                    break
+        for line in self.price_rule_ids.filtered(lambda rule: not (categ_ids - rule.categ_ids)):
+            test = safe_eval(line.variable + line.operator + str(line.max_value), price_dict)
+            if test:
+                price = line.list_base_price + line.list_price * price_dict[line.variable_factor]
+                criteria_found = True
+                break
         if not criteria_found:
             raise UserError(_("No price rule matching this order; delivery cost cannot be computed."))
-        print("price >>>>>>>>>>>>>>>>>>", price)
         return price
-
-    # def _get_price_from_picking(self, total, weight, volume, quantity):
-    #     print("_get_price_from_picking >>>>>>>>>>>>>", self, total, weight, volume, quantity, self.env.context)
-    #     import inspect
-    #     for i in inspect.stack():
-    #         print("i >>>>>>>>>>>>>>>>", i)
-    #     price = 0.0
-    #     criteria_found = False
-    #     price_dict = self._get_price_dict(total, weight, volume, quantity)
-    #     if self.free_over and total >= self.amount:
-    #         return 0
-    #     print("self.price_rule_ids >>>>>>>>>>>>>>", self.price_rule_ids)
-    #     for line in self.price_rule_ids:
-    #         test = safe_eval(line.variable + line.operator + str(line.max_value), price_dict)
-    #         if test:
-    #             price = line.list_base_price + line.list_price * price_dict[line.variable_factor]
-    #             criteria_found = True
-    #     if not criteria_found:
-    #         raise UserError(_("No price rule matching this order; delivery cost cannot be computed."))
-
-    #     return price
