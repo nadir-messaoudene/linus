caaecccd1d88861d2d780cce9a94bdb60b2d08f4
Toan Truong <toan@syncoria.com>
2022-09-28 13:49:11 -0400

    [ADD] linus_sale: pricelist: combination

--- linus_sale/models/sale.py
+++ linus_sale/models/sale.py
@@ -60,14 +60,13 @@ class SaleOrder(models.Model):
             return
         for rec in to_update:
             rec._get_delivery_tracking()
-    
+
+#INHERTI PRICELIST for LINUS PROMOTION    
     @api.onchange('order_line')
     def _onchange_order_line_discount(self):
         for line in self.order_line:
             line._onchange_discount()
 
-
-#INHERTI PRICELIST for LINUS PROMOTION
 class PricelistItem(models.Model):
     _inherit = "product.pricelist.item"
     @api.depends('applied_on', 'categ_id', 'product_tmpl_id', 'product_id', 'compute_price', 'fixed_price', \
@@ -90,6 +89,62 @@ class PricelistItem(models.Model):
             else:
                 item.price = _("%(percentage)s %% discount and %(price)s surcharge", percentage=item.price_discount, price=item.price_surcharge)
 
+    #Update Min_Quantity with Combination for Comparision
+    def min_quantity_updated(self):
+        min_quantity_updated = self.min_quantity
+        #case: category in combine but not apply by quantity
+        temp = 0
+        combination_obj = self.pricelist_id.combination_ids.search([('item_id', '=', self.id)])
+        if combination_obj:
+            combination = self.pricelist_id.combination_ids.search([('combine_number', '=', combination_obj.combine_number)])
+            for item in combination:
+                temp += item.item_id.min_quantity
+        if temp > 0:
+            return temp
+        return min_quantity_updated
+
+    def _is_applicable_for(self, product, qty_in_product_uom):
+        self.ensure_one()
+        product.ensure_one()
+        res = True
+
+        is_product_template = product._name == 'product.template'
+        if self.min_quantity:
+            if self.pricelist_id.apply_over and (product.categ_id in self.pricelist_id.combination_ids.item_id.categ_id or product.categ_id in self.pricelist_id.combination_ids.item_id.categ_id.child_id):
+                self_min_quantity_updated = self.min_quantity_updated()
+                if qty_in_product_uom < self_min_quantity_updated:
+                    res = False
+            else:
+                if qty_in_product_uom < self.min_quantity:
+                    res = False
+
+        elif self.categ_id:
+            # Applied on a specific category
+            cat = product.categ_id
+            while cat:
+                if cat.id == self.categ_id.id:
+                    break
+                cat = cat.parent_id
+            if not cat:
+                res = False
+        else:
+            # Applied on a specific product template/variant
+            if is_product_template:
+                if self.product_tmpl_id and product.id != self.product_tmpl_id.id:
+                    res = False
+                elif self.product_id and not (
+                    product.product_variant_count == 1
+                    and product.product_variant_id.id == self.product_id.id
+                ):
+                    # product self acceptable on template if has only one variant
+                    res = False
+            else:
+                if self.product_tmpl_id and product.product_tmpl_id.id != self.product_tmpl_id.id:
+                    res = False
+                elif self.product_id and product.id != self.product_id.id:
+                    res = False
+
+        return res
 class PricelistItemCombination(models.Model):
     _name = "product.pricelist.item.combination"
     _order = 'combine_number ASC'
@@ -144,7 +199,7 @@ class Pricelist(models.Model):
             #One Rule exits only one Combine Number.
             for combine_item in combine_item_list:
                 check_detail_by_item = combination_ids.filtered(lambda l: l.item_id == combine_item)
-                if len(check_detail_by_item) == len(combine_number_list):
+                if len(check_detail_by_item) > 1:
                     raise UserError('Please re-check the combination. Note: One Rule exits only one Combine Number.')
 
         return True
@@ -155,23 +210,22 @@ class SaleOrderLine(models.Model):
     @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self):
         print("_onchange_discount")
-        def combine_applied(lines, categories):
-            if lines.product_id.categ_id == categories:
+        def combine_applied(order_lines, categories):
+            if self.product_id.categ_id in categories or self.product_id.categ_id in categories.child_id:
                 return True
             return False
 
-        def calculate_product_qty(lines, pricelist=False, combine_applied=False):
+        def calculate_product_qty(order_lines, pricelist=False, combine_applied=False):
             qty = 0
             if pricelist.combination_ids and combine_applied:
-                for line in lines:
-                   if line.product_id.categ_id in pricelist.combination_ids.categ_id:
-                        qty += line.product_uom_qty 
+                for order_line in order_lines:
+                   if order_line.product_id.categ_id in pricelist.combination_ids.item_id.categ_id or order_line.product_id.categ_id in pricelist.combination_ids.item_id.categ_id.child_id:
+                        qty += order_line.product_uom_qty
             else:
-                for line in lines:
-                    if line.product_id.categ_id == self.product_id.categ_id:
-                        qty += line.product_uom_qty
+                for order_line in order_lines:
+                    if order_line.product_id.categ_id == self.product_id.categ_id or order_line.product_id.categ_id.parent_id == self.product_id.categ_id.parent_id:
+                        qty += order_line.product_uom_qty
             return qty
-        
 
         if not (self.product_id and self.product_uom and
                 self.order_id.partner_id and self.order_id.pricelist_id and
@@ -184,7 +238,7 @@ class SaleOrderLine(models.Model):
         if self.order_id.pricelist_id.apply_over:
             qty = calculate_product_qty(self.order_id._get_update_prices_lines(), 
                                         self.order_id.pricelist_id,
-                                        combine_applied(self.order_id._get_update_prices_lines(), self.order_id.pricelist_id.combination_ids.categ_id))
+                                        combine_applied(self.order_id._get_update_prices_lines(), self.order_id.pricelist_id.combination_ids.item_id.categ_id))
 
         product = self.product_id.with_context(
             lang=self.order_id.partner_id.lang,
