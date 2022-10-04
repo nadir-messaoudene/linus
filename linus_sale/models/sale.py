3cefdefc37a1b2a1c015dc6931fe3329213b489c
Toan Truong <toan@syncoria.com>
2022-10-04 10:06:53 -0400

    [MODIFY][Ticket#455] linus_sale: filter not done backorders

--- linus_sale/models/sale.py
+++ linus_sale/models/sale.py
@@ -25,38 +25,28 @@ class SaleOrder(models.Model):
         ('push_3pl', 'Pushed to 3PL'),
         ('done', 'Done'),
         ('cancel', 'Cancelled')
-        ], string='Delivery Status', default='none', compute='_get_delivery_status', store=True)
-    count_backorder = fields.Integer('Back Orders Count', compute='_get_delivery_status', store=True)
+        ], string='Delivery Status', default='none', compute='_action_get_delivery', store=True)
+    count_backorder = fields.Integer('Back Orders Count', compute='_action_get_delivery', store=True)
 
-    last_carrier_tracking_ref = fields.Char(string='Tracking Reference',compute='_get_delivery_tracking', store=True)
+    last_carrier_tracking_ref = fields.Char(string='Tracking Reference', compute='_action_get_delivery', store=True)
 
-    @api.depends('picking_ids.state')
-    def _get_delivery_status(self):
-        print('_get_delivery_status')
+    @api.depends('picking_ids.carrier_tracking_ref', 'picking_ids.state')
+    def _action_get_delivery(self):
         for order in self:
             pickings = order.picking_ids.filtered(lambda l: l.picking_type_id.code != 'internal')
-            backorders = pickings.backorder_ids
-            pickings = sorted(pickings, key=lambda x: x.id)
-            if len(pickings) > 0:
-                order.delivery_status = pickings[-1].state
-            else: 
-                order.delivery_status = 'none'
-            
+            backorders = pickings.backorder_ids.filtered(lambda l: l.state not in ('done', 'cancel'))
             order.count_backorder = len(backorders)
-            
 
-    @api.depends('picking_ids.carrier_tracking_ref')
-    def _get_delivery_tracking(self):
-        for order in self:
-            pickings = order.picking_ids.filtered(lambda l: l.picking_type_id.code != 'internal')
             if len(pickings) > 0:
                 order.last_carrier_tracking_ref = pickings[-1].carrier_tracking_ref
+                order.delivery_status = pickings[-1].state
             else: 
                 order.last_carrier_tracking_ref = ''
+                order.delivery_status = 'none'
 
     def action_update(self):
         to_update = self.env['sale.order'].search([])
         if not to_update:
             return
         for rec in to_update:
-            rec._get_delivery_tracking()
+            rec._action_get_delivery()
