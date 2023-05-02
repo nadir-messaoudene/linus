7840956f96c835852b396055ecbc1228f8ad4482
DUC SYNCORIA <duc@syncoria.com>
2023-05-01 23:40:28 -0400

    [UPDATE] syncoria_resolvepay: move to new API

--- syncoria_resolvepay/models/account_move.py
+++ syncoria_resolvepay/models/account_move.py
@@ -13,17 +13,46 @@ class Invoice(models.Model):
     resolvepay_charge_id = fields.Char(string='ResolvePay Charge Id')
     available_credit = fields.Integer(string='Available Credit', related='partner_id.available_credit')
 
+    # def action_update_resolve_pay(self):
+    #     to_update = self.env['account.move'].search([('resolvepay_invoice_id', '!=', None), ('state', '=', 'posted'), ('payment_state', 'in', ('not_paid', 'in_payment', 'partial'))])
+    #     _logger.info("Invoices info =====> %s", to_update)
+    #     if not to_update:
+    #         return
+    #     for rec in to_update:
+    #         try:
+    #             time.sleep(0.5)
+    #             rec.resolvepay_fetch_invoice()
+    #         except:
+    #             continue
+
     def action_update_resolve_pay(self):
-        to_update = self.env['account.move'].search([('resolvepay_invoice_id', '!=', None), ('state', '=', 'posted'), ('payment_state', 'in', ('not_paid', 'in_payment', 'partial'))])
-        _logger.info("Invoices info =====> %s", to_update)
-        if not to_update:
-            return
-        for rec in to_update:
-            try:
-                time.sleep(0.5)
-                rec.resolvepay_fetch_invoice()
-            except:
+        date_params_start = fields.Datetime.now().strftime("%Y-%m-%dT00:00:00.000Z")
+        date_params_end = fields.Datetime.now().strftime("%Y-%m-%dT23:59:59.000Z")
+        today_date = fields.Date.today()
+        resolvepay_instance = self.env['resolvepay.instance'].search([('name', '=', 'ResolvePay')])
+        url = resolvepay_instance.instance_baseurl + 'payouts'
+        complete_url = url + '?filter[expected_by][gte]=' + date_params_start + '&filter[expected_by][lte]=' + date_params_end
+        res = resolvepay_instance.get_data(complete_url)
+        _logger.info(res)
+        payouts = res.get('data').get('results')
+        for payout in payouts:
+            _logger.info(payout)
+            if payout.get('status') in ('canceled', 'failed'):
                 continue
+            payment_date = payout.get('expected_by')
+            payout_id = payout.get('id')
+            url = resolvepay_instance.instance_baseurl + 'payout-transactions?filter[payout_id]=' + payout_id
+            res = resolvepay_instance.get_data(url)
+            if res.get('data'):
+                payout_transactions = res.get('data').get('results')
+                for payout_transaction in payout_transactions:
+                    _logger.info(payout_transaction)
+                    invoice_id = payout_transaction.get('invoice_id', False)
+                    invoice = self.search([('resolvepay_invoice_id', '=', invoice_id)], limit=1)
+                    if not invoice:
+                        _logger.info('Cannot find RP invoice: ' + invoice_id)
+                    if invoice.state == 'posted' and invoice.payment_state in ('partial', 'not_paid'):
+                        self.register_resolvepay_payment(invoice, payout_transaction, payment_date)
 
     def create_invoice_resolvepay(self):
         for record in self:
@@ -61,6 +90,49 @@ class Invoice(models.Model):
             else:
                 raise UserError('There is no ResolvePay instance')
 
+    def get_payout_info(self, payout_id):
+        resolvepay_instance = self.env['resolvepay.instance'].search([('name', '=', 'ResolvePay')])
+        url = resolvepay_instance.instance_baseurl + 'payouts/' + payout_id
+        res = resolvepay_instance.get_data(url)
+        return res.get('data')
+
+    def register_resolvepay_payment(self, invoice, payout_transaction, arrive_date):
+        payout_transaction_id = payout_transaction.get('id')
+        payout_id = payout_transaction.get('payout_id')
+        amount_gross = payout_transaction.get('amount_gross')
+        amount_fee = payout_transaction.get('amount_fee')
+        amount_net = payout_transaction.get('amount_net')
+        payment_type = payout_transaction.get('type')
+        invoice_id = payout_transaction.get('invoice_id', False)
+        payment_id = self.env['account.payment'].search([('rp_payout_transaction_id', '=', payout_transaction_id)])
+        if payment_id:
+            return
+        if payment_type not in ('advance', 'payment'):
+            return
+        if amount_gross > invoice.amount_residual:
+            return
+        if invoice_id == invoice.resolvepay_invoice_id and invoice.payment_state in ('not_paid', 'partial'):
+            journal = self.env['account.journal'].search([('code', '=', 'RSP')])
+            if not journal:
+                raise ValidationError('Can not find Resolve Pay journal')
+            payment_dict = {
+                'journal_id': journal.id,
+                'amount': amount_gross,
+                'payment_date': arrive_date,
+            }
+            payment_method_line_id = journal.inbound_payment_method_line_ids
+            if payment_method_line_id:
+                payment_dict['payment_method_line_id'] = payment_method_line_id[0].id
+                pmt_wizard = self.env['account.payment.register'].with_context(
+                    active_model='account.move', active_ids=invoice.ids).create(payment_dict)
+                new_payment_id = pmt_wizard._create_payments()
+                new_payment_id.write({'rp_payout_transaction_id': payout_transaction_id,
+                                      'rp_payout_id': payout_id,
+                                      'rp_payout_transaction_type': payment_type,
+                                      'rp_payout_transaction_amount_gross': amount_gross,
+                                      'rp_payout_transaction_amount_fee': amount_fee,
+                                      'rp_payout_transaction_amount_net': amount_net})
+
     def resolvepay_fetch_invoice(self):
         for invoice in self:
             if invoice.state == 'cancel':
@@ -68,133 +140,20 @@ class Invoice(models.Model):
             if invoice.state == 'draft':
                 raise UserError('This invoice is not confirmed '+invoice.name)
             resolvepay_instance = self.env['resolvepay.instance'].search([('name', '=', 'ResolvePay')])
-            url = resolvepay_instance.instance_baseurl + 'invoices'
-            complete_url = url + '/' + invoice.resolvepay_invoice_id
+            url = resolvepay_instance.instance_baseurl + 'payout-transactions?filter[invoice_id]='
+            complete_url = url + invoice.resolvepay_invoice_id
             res = resolvepay_instance.get_data(complete_url)
-            if res.get('data'):
-                data = res.get('data')
-                _logger.info("Invoice data =====> %s", data)
+            if res.get('data').get('results'):
+                data = res.get('data').get('results', [])
+                _logger.info("Payout Transactions data =====> %s", data)
                 try:
-                    if data.get('advanced'):
-                        journal = self.env['account.journal'].search([('code', '=', 'RSP')])
-                        if not journal:
-                            raise ValidationError('Can not find Resolve Pay journal')
-                        if journal:
-                            payment_dict = {
-                                'journal_id': journal.id,
-                                'amount': data.get('amount_advance'),
-                                'payment_date': data.get('advanced_at'),
-                                'partner_id': invoice.partner_id.id,
-                                'resolvepay_payment_date': data.get('advanced_at')
-                            }
-                            payment_method_line_id = journal.inbound_payment_method_line_ids
-                            if payment_method_line_id:
-                                payment_dict['payment_method_line_id'] = payment_method_line_id[0].id
-                            if invoice.payment_state == 'not_paid':
-                                pmt_wizard = self.env['account.payment.register'].with_context(
-                                    active_model='account.move', active_ids=invoice.ids).create(payment_dict)
-                                payment = pmt_wizard.action_create_payments()
-                                print("===============>", payment)
-                                _logger.info(
-                                    "Payment-{} Posted for Invoice-{}".format(payment, invoice))
-                                invoice.partner_id.fetch_customer_resolvepay()
-                    if data.get('amount_paid') and data.get('payout_fully_paid_at'):
-                        journal = self.env['account.journal'].search([('code', '=', 'RSP')])
-                        if not journal:
-                            raise ValidationError('Can not find Resolve Pay journal')
-                        if journal:
-                            payment_dict = {
-                                'journal_id': journal.id,
-                                'amount': data.get('amount_paid'),
-                                'payment_date': data.get('payout_fully_paid_at') if data.get('payout_fully_paid_at') else date.today(),
-                                'partner_id': invoice.partner_id.id,
-                                'resolvepay_payment_date': data.get('payout_fully_paid_at') if data.get('payout_fully_paid_at') else date.today()
-                            }
-                            payment_method_line_id = journal.inbound_payment_method_line_ids
-                            if payment_method_line_id:
-                                payment_dict['payment_method_line_id'] = payment_method_line_id[0].id
-                            if invoice.payment_state in ['not_paid', 'partial']:
+                    for payout_transaction in data:
+                        _logger.info(payout_transaction)
+                        payout_id = payout_transaction.get('payout_id')
+                        payout_info = self.get_payout_info(payout_id)
+                        payout_arrive_date = payout_info.get('expected_by')
+                        self.register_resolvepay_payment(invoice, payout_transaction, payout_arrive_date)
 
-                                payment_vals = json.loads(invoice.invoice_payments_widget)
-                                total_amount = 0
-                                for payment in payment_vals.get('content'):
-                                    payment_id = self.env['account.payment'].browse(payment['account_payment_id'])
-                                    total_amount += payment_id.amount
-                                if data.get('amount_paid') > total_amount:
-                                    # payment_dict['communication'] = pay_id.ref.split('-')[0]
-                                    payment_dict['amount'] = data.get('amount_paid') - total_amount
-                                    pmt_wizard = self.env['account.payment.register'].with_context(
-                                        active_model='account.move', active_ids=invoice.ids).create(payment_dict)
-                                    payment = pmt_wizard.action_create_payments()
-                                    print("===============>", payment)
-                                    _logger.info(
-                                        "Payment-{} Posted for Invoice-{}".format(payment, invoice))
-                                    invoice.partner_id.fetch_customer_resolvepay()
-                    # if data.get('amount_refunded'):
-                    #     reverse_moves = self.env['account.move'].search([('reversed_entry_id', '=', invoice.id), ('state', '=', 'posted')])
-                    #     if invoice.payment_state in ['paid', 'in_payment', 'partial']:
-                    #         journal = self.env['account.journal'].search([('code', '=', 'RSP')])
-                    #         if not reverse_moves:
-                    #             wizard_vals = {
-                    #                 'refund_method': 'refund',
-                    #                 'date': date.today(),
-                    #                 'journal_id': invoice.journal_id.id
-                    #             }
-                    #             reversal_wizard = self.env['account.move.reversal'].with_context(
-                    #                 active_model='account.move',
-                    #                 active_ids=invoice.ids).create(wizard_vals)
-                    #             reversal_wizard.sudo().reverse_moves()
-                    #             refund_move_id = self.env['account.move'].search(
-                    #                 [('reversed_entry_id', '=', invoice.id), ('state', '=', 'draft')])
-                    #             refund_move_id.resolvepay_invoice_id = ''
-                    #             refund_move_id.invoice_line_ids.with_context(check_move_validity=False).unlink()
-                    #             refund_move_id.invoice_line_ids = [(0, 0, {'move_id': refund_move_id.id,
-                    #                                                    'name': "Refund",
-                    #                                                    'quantity': 1,
-                    #                                                    'price_unit': data.get('amount_refunded')})]
-                    #         else:
-                    #             total_refund = 0
-                    #             for refund in reverse_moves:
-                    #                 total_refund += refund.amount_total
-                    #             if data.get('amount_refunded') > total_refund:
-                    #                 wizard_vals = {
-                    #                     'refund_method': 'refund',
-                    #                     'date': date.today(),
-                    #                     'journal_id': invoice.journal_id.id
-                    #                 }
-                    #                 reversal_wizard = self.env['account.move.reversal'].with_context(
-                    #                     active_model='account.move',
-                    #                     active_ids=invoice.ids).create(wizard_vals)
-                    #                 reversal_wizard.sudo().reverse_moves()
-                    #                 refund_move_id = self.env['account.move'].search(
-                    #                     [('invoice_origin', '=', data.get('order_number')),
-                    #                      ('move_type', "=", "out_refund"), ('state', '=','draft')], order='id desc', limit=1)
-                    #                 refund_move_id.resolvepay_invoice_id = ''
-                    #                 refund_move_id.invoice_line_ids.with_context(check_move_validity=False).unlink()
-                    #                 refund_move_id.invoice_line_ids = [(0,0, {'move_id': refund_move_id.id,
-                    #                                                        'name': "Refund",
-                    #                                                        'quantity': 1,
-                    #                                                        'price_unit': data.get('amount_refunded') - total_refund})]
-                    #         if refund_move_id and refund_move_id.state == 'draft':
-                    #             refund_move_id.action_post()
-                    #             _logger.info("Credit Note-{} Posted for Invoice-{}".format(refund_move_id, invoice))
-                    #         if not journal:
-                    #             raise ValidationError('Can not find Resolve Pay journal')
-                    #         if journal and refund_move_id:
-                    #             payment_dict = {
-                    #                 'journal_id': journal.id,
-                    #                 'payment_date': date.today(),
-                    #             }
-                    #             payment_method_line_id = journal.outbound_payment_method_line_ids
-                    #             if payment_method_line_id:
-                    #                 payment_dict['payment_method_line_id'] = payment_method_line_id[0].id
-                    #             pmt_wizard = self.env['account.payment.register'].with_context(
-                    #                 active_model='account.move', active_ids=refund_move_id.ids).create(
-                    #                 payment_dict)
-                    #             payment = pmt_wizard.action_create_payments()
-                    #             print("===============>", payment)
-                    #             _logger.info(
-                    #                 "Payment-{} Posted for Credit Note-{}".format(payment, refund_move_id))
                 except Exception as e:
                     _logger.info("Exception-{}".format(e))
                     raise ValidationError(e)
--- syncoria_resolvepay/models/account_payment.py
+++ syncoria_resolvepay/models/account_payment.py
@@ -6,14 +6,27 @@ class AccountPaymentRegister(models.TransientModel):
 
     resolvepay_payment_date = fields.Char("ResolvePay payment datetime")
 
-    def _create_payment_vals_from_wizard(self):
-        # OVERRIDE
-        payment_vals = super()._create_payment_vals_from_wizard()
-        payment_vals['resolvepay_payment_date'] = self.resolvepay_payment_date
-        return payment_vals
+    # def _create_payment_vals_from_wizard(self):
+    #     # OVERRIDE
+    #     payment_vals = super()._create_payment_vals_from_wizard()
+    #     payment_vals['resolvepay_payment_date'] = self.resolvepay_payment_date
+    #     return payment_vals
 
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
-    resolvepay_payment_date = fields.Char("ResolvePay payment datetime")
+    resolvepay_payment_date = fields.Char("Resolve Pay payment datetime")
+    rp_payout_transaction_id = fields.Char("Resolve Pay Payout Transaction Id")
+    rp_payout_id = fields.Char("Resolve Pay Payout Id")
+    rp_payout_transaction_type = fields.Selection(selection=[('advance', 'advance'),
+                                                             ('payment', 'payment'),
+                                                             ('refund', 'refund'),
+                                                             ('monthly_fee', 'monthly_fee'), ('annual_fee', 'annual_fee'),
+                                                             ('non_advanced_invoice_fee', 'non_advanced_invoice_fee'),
+                                                             ('merchant_payment', 'merchant_payment'),
+                                                             ('mdr_extension', 'mdr_extension'),
+                                                             ('credit_note', 'credit_note')], string='Resolve Pay Transaction Type')
+    rp_payout_transaction_amount_gross = fields.Float('amount_gross')
+    rp_payout_transaction_amount_fee = fields.Float('amount_fee')
+    rp_payout_transaction_amount_net = fields.Float('amount_net')
