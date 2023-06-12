f09f24d7113449550748870a2039c9ab31b918b0
DUC SYNCORIA <duc@syncoria.com>
2023-06-12 16:29:59 -0400

    [UPDATE] linus_shipping_base_product_category: using default delivery set on contact for website regardless published or not

--- linus_shipping_base_product_category/models/sale_order.py
+++ linus_shipping_base_product_category/models/sale_order.py
@@ -11,3 +11,9 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     delivery_carrier_desc = fields.Text()
+
+    def _get_delivery_methods(self):
+        _logger.info('_get_delivery_methods inherit')
+        if self.partner_id.property_delivery_carrier_id:
+            return self.partner_id.property_delivery_carrier_id.sudo()
+        return super(SaleOrder, self)._get_delivery_methods()
